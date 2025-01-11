daaaaeb5294b9b9c911b6700521c605706a9dff6
andriesfc <andries.fc@gmail.com>
2025-01-11 22:47:43 +0200

    fix for windows build.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,12 @@
 #define fseeko _fseeki64
 #endif
 
+#ifdef _MSC_VER
+//not #if defined(_WIN32) || defined(_WIN64) because we have strncasecmp in mingw
+#define strncasecmp _strnicmp
+#define strcasecmp _stricmp
+#endif
+
 /*
  * WUX file structure (v1.0):
 		[Header]
